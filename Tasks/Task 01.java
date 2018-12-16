86740dd1c5840818e46948dc26709adfde38cb1d
JulianaAnt <julianaant.c@mail.ru>
2018-12-16 19:09:10 +0300

    MY FIRST HOMEWORK

---
+++ Tasks/Task 01.java
@@ -0,0 +1,15 @@
+class Task01 {
+	public static void main(String[] args) {
+		String a = "5000 - 3 ";
+		String b = "2000 - 0 ";
+		String c = "1000 - 0 ";
+		String d = "500 - 1 ";
+		String e = "200 - 0 ";
+		String f = "100 - 3 ";
+		String g = "50 - 1 ";
+		String h = "10 - 1 ";
+		String i = "5 - 1 ";
+		String j = "1 - 2 ";
+			System.out.printIn(a + b + c + d + e + f + g + h + i + j);
+	}
+}
